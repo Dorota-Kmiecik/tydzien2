5fd64fb6bd5fd04ec0b52c55345fac9ff4eda697
Dorota Kmiecik <dorota.kmiecik87@gmail.com>
2023-06-09 16:13:19 +0200

    Adding new tasks

---
+++ Tydzien2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tydzien2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zadanie 1:
+            string firstName;
+            string lastName;
+            char sex;
+            uint pesel;
+            ulong employerNumber;
+
+            //Zadanie 2
+            char x = 'A';
+            char y = 'B';
+            char z = 'C';
+
+            Console.WriteLine($"{z}, {y}, {x}");
+
+            //Zadanie 3
+            int lenght = 4;
+            int width = 7; 
+
+            int lenghtPow = (int)Math.Pow( lenght, 2 );
+            int widthPow = (int)Math.Pow( width, 2 );
+
+            double diagonal = Math.Sqrt( lenghtPow + widthPow);
+
+            Console.WriteLine(diagonal);
+
+            // Zadanie 4
+            int number1 = 10;
+            double number2 = 12.5;
+            string name = "Szkola Dotneta";
+
+            // Zadanie 5
+
+            Console.WriteLine("Podaj swoje imie");
+            string firstName1 = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoje nazwisko");
+            string lastName1 = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoja wage");
+            double weight = double.Parse( Console.ReadLine() );
+
+            Console.WriteLine("Podaj swoj numer telefonu");
+            uint phoneNumber = uint.Parse( Console.ReadLine() );
+
+
+
+
+
+
+
+
+        }
+    }
+}
